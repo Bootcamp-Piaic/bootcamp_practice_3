7b59d6372b6e1519677d7e76eee7fedcb705ad33
Owais Mansha <awaismansha1998@gmail.com>
2020-09-21 16:30:56 +0500

    making correction

--- src/numberReducer.js
+++ src/numberReducer.js
@@ -2,8 +2,10 @@ const numberReducer = (state,action)=>{
     switch(action.type){
         case 'increment':
             return state + action.val
-        case 'decrement':;
+        case 'decrement':
             return state - action.val
+		default:
+			throw new Error();
     }
 }
 export default numberReducer
